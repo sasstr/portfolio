6c43576575929074f009b6cf2afea5b94edb462d
Alexander Strakhovenko <sasstr@gmail.com>
2022-04-15 23:46:49 +0300

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,33 @@
+# Портфолио @sasstr&#8251; Александр
+
+## <b>Для запуска проекта: </b> 
+* Устанвовить npm зависимости ```npm i```
+* Запустить проект в режиме разработки ```npm start ```
+* Собрать проект ```npm build ```
+<hr>
+<img src="build/img/sasstr.jpg" alt="Моя фотка" width="260" >
+<hr>
+
+## <b>Навыки</b>
+
+### Фронтенд
+* HTML, CSS, JS
+* Git GitHub
+* Gulp
+* SVG
+* Scss
+* Canvas
+
+### Бэкенд
+* PHP
+* WordPress
+
+### Разное
+
+* Интернет-маркетинг
+* Поисковое продвижение
+* English
+* Менеджмент
+<hr>
+<img src="build/img/web-design.jpeg" alt="Заставка - код" width="700" >
+
---
+++ source/js/script.js
@@ -0,0 +1,68 @@
+const siteList = document.querySelector('.site-list');
+const specialistWrapper = document.querySelector('.specialist-wrapper');
+const alibraWrapper = document.querySelector('.alibra-wrapper');
+const modal = document.querySelector('.modal');
+const modalImg = modal.querySelector('.modal-content');
+const captionText = modal.querySelector('#caption');
+const closeBtn = modal.querySelector('.close');
+
+/** Функция показывает модальное окно и помещает в него картинку, по которой кликает пользователь.
+ * 
+ * @param {string} event 
+ * @param {string} className 
+ * @param {number} widthImage 
+ * @returns {void}
+ */
+const showModal = function (event, className, widthImage = 500) {
+  
+  if(event.target.classList.contains(className)){
+    event.preventDefault();
+    modal.style.display = "block";
+    modalImg.width = widthImage;
+    modalImg.src = event.target.src;
+    captionText.textContent = event.target.alt;
+  }
+}
+
+siteList.onclick = function (evt) {
+  showModal(evt, 'card-image') 
+}
+
+specialistWrapper.onclick = function (evt) {
+  showModal(evt, 'specialist-image', 1000) 
+}
+
+alibraWrapper.onclick = function (evt) {
+  showModal(evt, 'alibra-image') 
+}
+
+/** Функция скрывает модальное окно
+ * 
+ * @param {*} evt 
+ * @returns {void}
+ */
+const closeModal = function (evt) {
+  evt.preventDefault();
+  modal.style.display = "none";
+  
+}
+
+const clickBtnHandler = function (e) {
+  closeModal(e)
+};
+
+const modalContainerClickHandler = function (evt) {
+  if(evt.target.classList.contains('modal')){
+    closeModal(evt)
+  }
+};
+
+const btnKeyupHandler = function  (evt) {
+  if (evt.keyCode == 27){
+    closeModal(evt)
+  }
+};
+
+document.addEventListener('keyup', btnKeyupHandler);
+document.addEventListener('click', modalContainerClickHandler);
+closeBtn.addEventListener('click', clickBtnHandler);
